085dcebc3f45c77b2558e7ae18d2a6027fc49164
eons44 <s.oshannon@brain2bot.com>
2019-04-11 22:50:17 -0700

    accounted for drawing without replacement

--- calcWin.py
+++ calcWin.py
@@ -2,26 +2,42 @@ import plotly
 import plotly.graph_objs as go
 
 numCards = 99
-pColorUntap = 5 / numCards
-pColorFixer = 4 / numCards
-pGenericUntap = 2 / numCards
-pDevotedDruid = 1 / numCards
-pPlusPlus = 1 / numCards
-pMana = 6 / numCards
-pDraw = 7 / numCards
+nColorUntap = 5
+nColorFixer = 4
+nGenericUntap = 2
+nDevotedDruid = 1
+nPlusPlus = 1
+nMana = 6
+nDraw = 7
+
+#calculate probability of drawing without replacement
+def ProbabilityOfDrawing(numOfCard, totalCards, cardsDrawn):
+    ret = 0
+    for c in range(0, cardsDrawn):
+        ret += numOfCard / (totalCards - cardsDrawn)
+    return ret
 
 def CalcWin(cards):
+
+    #determine the probability of getting each type of card
+    pColorUntap = ProbabilityOfDrawing(nColorUntap, numCards, cards)
+    pColorFixer = ProbabilityOfDrawing(nColorFixer, numCards, cards)
+    pGenericUntap = ProbabilityOfDrawing(nGenericUntap, numCards, cards)
+    pDevotedDruid = ProbabilityOfDrawing(nDevotedDruid, numCards, cards)
+    pPlusPlus = ProbabilityOfDrawing(nPlusPlus, numCards, cards)
+    pMana = ProbabilityOfDrawing(nMana, numCards, cards)
+    pDraw = ProbabilityOfDrawing(nDraw, numCards, cards)
     
     #calculate probability of having an untap combo
-    pUntap = pColorUntap*cards * pColorFixer*cards
-    pUntap += pGenericUntap*cards 
-    pUntap += pDevotedDruid*cards * pPlusPlus*cards
+    pUntap = pColorUntap * pColorFixer
+    pUntap += pGenericUntap
+    pUntap += pDevotedDruid * pPlusPlus
 
     #infinite combo is having both mana and untap ability
-    pCombo = pUntap * pMana*cards
+    pCombo = pUntap * pMana
 
     #winning is having infinite mana and reusable draw power
-    pWin = pCombo * pDraw*cards
+    pWin = pCombo * pDraw
     return pWin
 
 turn = 1 #assume we go second, just subtract a turn if going first
@@ -48,9 +64,9 @@ plotly.offline.plot({
     "data": [go.Scatter(
         x=turns,
         y=pWins,
-        mode='markers+text',
+        mode='lines+markers',
 #        mode='lines+markers+text',
 #        text=winStrs,
         textposition='bottom right')],
     "layout": go.Layout(title="Probability of winning"),
-}, auto_open=True)
+}, auto_open=False)
