f1c20d241fde8c171a3ffcf220dfd02c7c1d2974
eons <eons44@users.noreply.github.com>
2019-04-12 05:34:36 -0700

    Fixes for stupidity
    This should still be checked for correctness

--- calcWin.py
+++ calcWin.py
@@ -12,10 +12,12 @@ nDraw = 7
 
 #calculate probability of drawing without replacement
 def ProbabilityOfDrawing(numOfCard, totalCards, cardsDrawn):
-    ret = 0
+    ret = 1.0
+    #print("calculating probability of ", numOfCard, " in ", cardsDrawn, " draws")
     for c in range(0, cardsDrawn):
-        ret += numOfCard / (totalCards - cardsDrawn)
-    return ret
+        inc =  (totalCards - c - numOfCard) / (totalCards - c)
+        ret *= inc
+    return 1.0 - ret
 
 def CalcWin(cards):
 
@@ -27,17 +29,22 @@ def CalcWin(cards):
     pPlusPlus = ProbabilityOfDrawing(nPlusPlus, numCards, cards)
     pMana = ProbabilityOfDrawing(nMana, numCards, cards)
     pDraw = ProbabilityOfDrawing(nDraw, numCards, cards)
+    print("probability of draw is ", pDraw)
     
     #calculate probability of having an untap combo
-    pUntap = pColorUntap * pColorFixer
-    pUntap += pGenericUntap
-    pUntap += pDevotedDruid * pPlusPlus
+    pNotUntap = 1.0 - pColorUntap * pColorFixer
+    pNotUntap *= 1.0 - pGenericUntap
+    pNotUntap *= 1.0 - pDevotedDruid * pPlusPlus
+    pUntap = 1.0 - pNotUntap
 
     #infinite combo is having both mana and untap ability
     pCombo = pUntap * pMana
 
     #winning is having infinite mana and reusable draw power
     pWin = pCombo * pDraw
+
+    print("probability of untap is ", pUntap)
+    print("probability of mana is ", pMana)
     return pWin
 
 turn = 1 #assume we go second, just subtract a turn if going first
@@ -50,7 +57,7 @@ pWins.append(CalcWin(7))
 winStrs = []
 winStrs.append("turn 0: " + str(pWins[0]))
 
-while(pWin < 0.99): #arbitrary cutoff for "good enough"
+while(pWin < 1.0): #arbitrary cutoff for "good enough"
     cards = 7+turn
     pWin = CalcWin(cards)
     winStr = "Turn" + str(turn) + " : " + str(pWin)
@@ -68,5 +75,5 @@ plotly.offline.plot({
 #        mode='lines+markers+text',
 #        text=winStrs,
         textposition='bottom right')],
-    "layout": go.Layout(title="Probability of winning"),
+    "layout": go.Layout(title="Probability of having winning combo"),
 }, auto_open=False)
